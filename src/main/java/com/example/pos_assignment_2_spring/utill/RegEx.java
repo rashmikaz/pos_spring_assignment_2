42f9a066dc6b30d0ebb0e0cdd6a6688c7e17b196
rashmikaz <phrashmika@gmail.com>
2024-10-15 03:13:44 +0530

    Add RegEx.java

---
+++ src/main/java/com/example/pos_assignment_2_spring/utill/RegEx.java
@@ -0,0 +1,16 @@
+package com.example.pos_assignment_2_spring.utill;
+
+import java.util.regex.Pattern;
+
+public class RegEx {
+    public static boolean customerIdMatcher(String customerId) {
+        String regexForCustomerID = "^CUS-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern regexPattern = Pattern.compile(regexForCustomerID);
+        return regexPattern.matcher(customerId).matches();
+    }
+    public static boolean itemCodeMatcher(String itemCode) {
+        String regexForItemCode = "^ITEM-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern regexPattern = Pattern.compile(regexForItemCode);
+        return regexPattern.matcher(itemCode).matches();
+    }
+}
