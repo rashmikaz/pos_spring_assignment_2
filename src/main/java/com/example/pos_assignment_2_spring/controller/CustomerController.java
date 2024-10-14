df6b92329998d593928d048d7035f1690217730e
rashmikaz <phrashmika@gmail.com>
2024-10-15 01:15:03 +0530

    customer save done

--- src/main/java/com/example/pos_assignment_2_spring/controller/CustomerController.java
+++ src/main/java/com/example/pos_assignment_2_spring/controller/CustomerController.java
@@ -4,6 +4,9 @@ package com.example.pos_assignment_2_spring.controller;
 import com.example.pos_assignment_2_spring.dto.Impl.CustomerDTO;
 import com.example.pos_assignment_2_spring.exception.DataPersistException;
 import com.example.pos_assignment_2_spring.service.CustomerService;
+import com.example.pos_assignment_2_spring.utill.AppUtill;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,20 +20,24 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+    private static Logger logger = LoggerFactory.getLogger(CustomerController.class);
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveCustomer(@RequestBody CustomerDTO customerDTO){
         try {
             customerService.saveCustomer(customerDTO);
+            logger.info("Customer Save Successfully");
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             e.printStackTrace();
+            logger.error("Customer save unsuccessful",e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         }
+
     }
 
 
--- src/main/java/com/example/pos_assignment_2_spring/utill/AppUtill.java
+++ src/main/java/com/example/pos_assignment_2_spring/utill/AppUtill.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public class AppUtill {
 
-    public static String generateCustomerId(){
-        return "CUS-" + UUID.randomUUID();
-    }
+//    public static String generateCustomerId(){
+//        return "CUS-" + UUID.randomUUID();
+//    }
 }
