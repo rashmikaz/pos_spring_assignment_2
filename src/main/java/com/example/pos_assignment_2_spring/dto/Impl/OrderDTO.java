b86906b85e96aebc86ac211a068c5a8341218457
rashmikaz <phrashmika@gmail.com>
2024-10-17 16:01:54 +0530

    API overview

--- src/main/java/com/example/pos_assignment_2_spring/dto/Impl/OrderDTO.java
+++ src/main/java/com/example/pos_assignment_2_spring/dto/Impl/OrderDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,5 +15,5 @@ public class OrderDTO implements OrderStatus {
     private String orderId;
     private String customerId;
     private LocalDate orderDate;
-//    private List<OrderDetailsDTO> orderDetails;
+    private List<OrderDetailsDTO> orderDetails;
 }
--- src/main/java/com/example/pos_assignment_2_spring/service/Impl/OrderServiceIMPL.java
+++ src/main/java/com/example/pos_assignment_2_spring/service/Impl/OrderServiceIMPL.java
@@ -1,6 +1,10 @@
 package com.example.pos_assignment_2_spring.service.Impl;
 
 import com.example.pos_assignment_2_spring.service.OrderService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class OrderServiceIMPL implements OrderService {
 }
